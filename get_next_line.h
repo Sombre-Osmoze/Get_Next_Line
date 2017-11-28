fc472bf3971deed1f8c21d17e6fd61be6fe5dbf5
Marcus Florentin <fmarcus28@gmx.fr>
2017-11-28 06:15:11 +0100

    Setting

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcusflorentin <marvin@42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/28 04:16:48 by marcusflo         #+#    #+#             */
+/*   Updated: 2017/11/28 04:16:55 by marcusflo        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
