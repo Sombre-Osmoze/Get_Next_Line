1466ab7471eb5b03f956d72c7e2a6eccb9e932a5
Marcus Florentin <fmarcus28@gmx.fr>
2018-06-14 02:28:46 +0200

    Cleaning

--- get_next_line.c
+++ get_next_line.c
@@ -31,58 +31,58 @@ static int	ft_stock_data(int fd, char *rest, t_ctrl *ctrl, size_t size)
 	return (-1);
 }
 
-static long    ft_read_line(const int fd, char **line, t_ctrl *ctrl, size_t res)
+static long	ft_read_line(const int fd, char **line, t_ctrl *ctrl, size_t res)
 {
-    char            tmp[BUFF_SIZE + 1];
-    long            i[3];
-    char            *buff;
+	char			tmp[BUFF_SIZE + 1];
+	long			i[3];
+	char			*buff;
 
-    ft_longset(i, 0, 3);
-    i[1] = BUFF_SIZE + 1;
-    while (i[1] == BUFF_SIZE + 1 && (i[0] = read(fd, tmp, BUFF_SIZE)) > 0)
-    {
-        i[1] = (long)ft_memichr(tmp, '\n', BUFF_SIZE);
-        if (i[1] == BUFF_SIZE + 1)
-            buff = ft_strnjoin(*line, res, tmp, i[0]);
-        else
-            buff = ft_strnjoin(*line, res, tmp, i[1]);
-        if (*line)
-            free(*line);
-        *line = buff;
-        res += (i[1] != BUFF_SIZE + 1) ? i[1] : i[0];
-        i[2] = i[0];
-    }
-    if (i[0] == -1)
-        return (-1);
-    if (i[1] < i[2] && tmp[i[1] + 1])
-        return (ft_stock_data(fd, &tmp[i[1] + 1], ctrl, i[2] - (i[1] + 1)));
-    return (i[2]);
+	ft_longset(i, 0, 3);
+	i[1] = BUFF_SIZE + 1;
+	while (i[1] == BUFF_SIZE + 1 && (i[0] = read(fd, tmp, BUFF_SIZE)) > 0)
+	{
+		i[1] = (long)ft_memichr(tmp, '\n', BUFF_SIZE);
+		if (i[1] == BUFF_SIZE + 1)
+			buff = ft_strnjoin(*line, res, tmp, i[0]);
+		else
+			buff = ft_strnjoin(*line, res, tmp, i[1]);
+		if (*line)
+			free(*line);
+		*line = buff;
+		res += (i[1] != BUFF_SIZE + 1) ? i[1] : i[0];
+		i[2] = i[0];
+	}
+	if (i[0] == -1)
+		return (-1);
+	if (i[1] < i[2] && tmp[i[1] + 1])
+		return (ft_stock_data(fd, &tmp[i[1] + 1], ctrl, i[2] - (i[1] + 1)));
+	return (i[2]);
 }
 
-static long    ft_get_buff(const int fd, char **line, t_ctrl *cl, long *rest)
+static long	ft_get_buff(const int fd, char **line, t_ctrl *cl, long *rest)
 {
-    size_t    lim;
-    size_t    *ref;
+	size_t	lim;
+	size_t	*ref;
 
-    lim = 0;
-    if (BUFF_SIZE > 1 && cl && ft_search_item(cl, &fd, 0, &ft_int_cmp))
-    {
-        ref = (size_t *)cl->last_ac->content_ref;
-        lim = ft_memichr((char *)cl->last_ac->content + ref[1], '\n',
+	lim = 0;
+	if (BUFF_SIZE > 1 && cl && ft_search_item(cl, &fd, 0, &ft_int_cmp))
+	{
+		ref = (size_t *)cl->last_ac->content_ref;
+		lim = ft_memichr((char *)cl->last_ac->content + ref[1], '\n',
 					cl->last_ac->content_size - ref[1]);
-        if (lim != cl->last_ac->content_size - ref[1] + 1)
-        {
-            *rest = 42;
-            *line = ft_memndup((char *)cl->last_ac->content + ref[1], lim);
-            ref[1] += lim + 1;
-        }
-        else
-            *line = ft_memndup((char *)cl->last_ac->content + ref[1], lim - 1);
-        if (lim == cl->last_ac->content_size + 1 - ref[1]
-                || ref[1] >= cl->last_ac->content_size)
-            ft_rm_item(cl, cl->last_ac->row);
-    }
-    return (lim);
+		if (lim != cl->last_ac->content_size - ref[1] + 1)
+		{
+			*rest = 42;
+			*line = ft_memndup((char *)cl->last_ac->content + ref[1], lim);
+			ref[1] += lim + 1;
+		}
+		else
+			*line = ft_memndup((char *)cl->last_ac->content + ref[1], lim - 1);
+		if (lim == cl->last_ac->content_size + 1 - ref[1]
+				|| ref[1] >= cl->last_ac->content_size)
+			ft_rm_item(cl, cl->last_ac->row);
+	}
+	return (lim);
 }
 
 /*
@@ -92,30 +92,30 @@ static long    ft_get_buff(const int fd, char **line, t_ctrl *cl, long *rest)
 ** @return Information sur le fichier
 */
 
-int            get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-    static t_ctrl    *ctrl;
-    long            res[2];
+	static t_ctrl	*ctrl;
+	long			res[2];
 
-    ft_bzero(res, sizeof(res));
-    if (fd > -1 && BUFF_SIZE >= 0 && line)
-    {
-        if (!ctrl)
-            ctrl = ft_init_ctrl();
-        *line = NULL;
-        res[1] = ft_get_buff(fd, line, ctrl, res);
-        if (res[0] != 42)
-            res[0] = ft_read_line(fd, line, ctrl, res[1]);
-        if (res[0] == -1 || (res[0] == 0 && ctrl->nb_item == 1
-                                && ft_search_item(ctrl, &fd, 0, &ft_int_cmp)))
-        {
-            ft_rm_list(ctrl);
-            ctrl = NULL;
-        }
-    }
-    else
-        return (-1);
-    if (res[0] >= 1)
-        res[0] = 1;
-    return ((int)res[0]);
+	ft_bzero(res, sizeof(res));
+	if (fd > -1 && BUFF_SIZE >= 0 && line)
+	{
+		if (!ctrl)
+			ctrl = ft_init_ctrl();
+		*line = NULL;
+		res[1] = ft_get_buff(fd, line, ctrl, res);
+		if (res[0] != 42)
+			res[0] = ft_read_line(fd, line, ctrl, res[1]);
+		if (res[0] == -1 || (res[0] == 0 && ctrl->nb_item == 1
+								&& ft_search_item(ctrl, &fd, 0, &ft_int_cmp)))
+		{
+			ft_rm_list(ctrl);
+			ctrl = NULL;
+		}
+	}
+	else
+		return (-1);
+	if (res[0] >= 1)
+		res[0] = 1;
+	return ((int)res[0]);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,12 +13,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1000000
+# define BUFF_SIZE 3
 
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/includes/libft.h"
 
-int            get_next_line(const int fd, char **line);
+int		get_next_line(const int fd, char **line);
 
 #endif
